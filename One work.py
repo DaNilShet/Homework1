fbe376eafe92b3bfe20599595361b015aae78bca
DaNilShet <danil.vip010@gmail.com>
2024-08-12 10:55:24 +0300

    Add files via upload

---
+++ One work.py
@@ -0,0 +1,12 @@
+#'1st program'
+print((9**0.5)*5)
+#'2st program'
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+#4ht program
+print("123.456")
+print(type(float("123.456")))
+print(123.456*10)
